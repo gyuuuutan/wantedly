e5031ea98aa92b97fc9dea29286e99b8275620ac
gyuuuutan <gyuuuutan.pei@gmail.com>
2017-02-23 17:26:14 +0900

    Create view of projects detail screen

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,15 @@
 class ProductsController < ApplicationController
 
+  before_action :set_products
+
   def index
+  end
+
+  def show
+  end
+
+  private
+  def set_products
     @company = Company.new
   end
 
