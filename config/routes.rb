b0a7f1e5dac10c9dbdd5c4731d78671ef6faec2f
jumpyoshim <jumpyoshim@gmail.com>
2017-02-23 17:26:14 +0900

    Create view of projects detail screen

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :companies
   resources :companies, only: [:index]
-  resources :products, only: [:index]
+  resources :products, only: [:index, :show]
   root "products#index"
 end
