f7a9d977e2d038a0a7a7833625154d22bbfee02a
jumpyoshim <jumpyoshim@gmail.com>
2017-03-01 21:53:00 +0900

    Setting members routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :companies
-  resources :companies, only: [:index]
+  resources :companies, only: [:index] do
+    resources :members, only: [:new, :create]
+  end
   resources :products
   root "products#index"
 end
